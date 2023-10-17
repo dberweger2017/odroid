161cb047621bb874ff100640b1f0d36461cfe6ba
dberweger2017 <dberweger@gmail.com>
2023-10-17 15:29:04 +0200

    Test3...

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
-import mraa
+import odroid_wiringpi as wpi
 import time
 
-# Initialize PWM on PWM pin (Check your board's pinout for the right pin number)
-pwm_pin = 3  # Change this to your actual PWM pin number
-servo = mraa.Pwm(pwm_pin)
+wpi.wiringPiSetup()
+wpi.pinMode(1, wpi.PWM_OUTPUT)
 
-# Enable the PWM pin
-servo.enable(True)
-
-# Set the PWM period in microseconds (this example uses 50Hz: 1/50 = 0.02s or 20000us)
-servo.period_us(20000)
-
-# Helper function to set the servo position
-def set_servo_position(servo, position):
-    # Convert the servo position (0-180) to duty cycle (usually between 0.03 and 0.12)
-    duty_cycle = 0.03 + ((position / 180.0) * (0.12 - 0.03))
-    servo.write(duty_cycle)
-
-# Example: Move servo from 0 to 180 degrees, then back to 0
-set_servo_position(servo, 0)
-time.sleep(1)
-set_servo_position(servo, 180)
-time.sleep(1)
-set_servo_position(servo, 0)
+while True:
+    for duty_cycle in range(50, 250, 5):  # Adjust values to match your servo's specifications
+        wpi.pwmWrite(1, duty_cycle)
+        time.sleep(0.02)
+    time.sleep(1)
+    for duty_cycle in range(250, 50, -5):  # Adjust values to match your servo's specifications
+        wpi.pwmWrite(1, duty_cycle)
+        time.sleep(0.02)
+    time.sleep(1)
