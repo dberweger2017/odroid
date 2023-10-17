68bf6849abdee865bb061c966c5f201cf161899e
dberweger2017 <dberweger@gmail.com>
2023-10-17 15:39:16 +0200

    debug

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ for i in range(1,40):
     wpi.wiringPiSetup()
     wpi.pinMode(i, wpi.PWM_OUTPUT)
 
-    while True:
+    for _ in range(5):
         for duty_cycle in range(50, 250, 5):  # Adjust values to match your servo's specifications
             wpi.pwmWrite(i, duty_cycle)
             time.sleep(0.02)
