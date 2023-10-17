247fc0c260245451040ea85ba2de5bdcb85ab76b
dberweger2017 <dberweger@gmail.com>
2023-10-17 15:36:47 +0200

    ...

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import odroid_wiringpi as wpi
 import time
 
-wpi.wiringPiSetup()
-wpi.pinMode(11, wpi.PWM_OUTPUT)
+for i in range(1,40):
+    wpi.wiringPiSetup()
+    wpi.pinMode(i, wpi.PWM_OUTPUT)
 
-while True:
-    for duty_cycle in range(50, 250, 5):  # Adjust values to match your servo's specifications
-        wpi.pwmWrite(11, duty_cycle)
-        time.sleep(0.02)
-    time.sleep(1)
-    for duty_cycle in range(250, 50, -5):  # Adjust values to match your servo's specifications
-        wpi.pwmWrite(11, duty_cycle)
-        time.sleep(0.02)
-    time.sleep(1)
+    while True:
+        for duty_cycle in range(50, 250, 5):  # Adjust values to match your servo's specifications
+            wpi.pwmWrite(11, duty_cycle)
+            time.sleep(0.02)
+        time.sleep(1)
+        for duty_cycle in range(250, 50, -5):  # Adjust values to match your servo's specifications
+            wpi.pwmWrite(11, duty_cycle)
+            time.sleep(0.02)
+        time.sleep(1)
