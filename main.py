28bcd375b08313636aac49e9557df6a2edd672d7
dberweger2017 <dberweger@gmail.com>
2023-10-17 15:48:44 +0200

    test

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import os
 for i in range(1,40):
     # Clear the terminal
     os.system('clear' if os.name == 'nt' else 'clear')
-    
+
     print(f"Trying pin {i}")
 
     wpi.wiringPiSetup()
     wpi.pinMode(i, wpi.PWM_OUTPUT)
 
-    for _ in range(5):
+    for _ in range(1):
         for duty_cycle in range(50, 250, 5):  # Adjust values to match your servo's specifications
             wpi.pwmWrite(i, duty_cycle)
             time.sleep(0.02)
