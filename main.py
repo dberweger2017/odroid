e257ae31dff97da2ae50d4cf2c15e59162e1f963
dberweger2017 <dberweger@gmail.com>
2023-10-17 16:01:19 +0200

    lossing it

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import odroid_wiringpi as wpi
 import time
 import os
-from tqdm import tqdm
 
-for i in tqdm(range(1,40)):
-    # Clear the terminal
-    os.system('clear' if os.name == 'nt' else 'clear')
+#connect to odroid
+# connecting to a pwm pin (GPIO18)
+p = 1
 
-    print(f"Trying pin {i}")
+wpi.wiringPiSetup()
+wpi.pinMode(p, wpi.PWM_OUTPUT)
 
-    wpi.wiringPiSetup()
-    wpi.pinMode(i, wpi.PWM_OUTPUT)
-
-    for _ in range(1):
-        for duty_cycle in range(50, 250, 5):  # Adjust values to match your servo's specifications
-            wpi.pwmWrite(i, duty_cycle)
-            time.sleep(0.02)
-        time.sleep(1)
-        for duty_cycle in range(250, 50, -5):  # Adjust values to match your servo's specifications
-            wpi.pwmWrite(i, duty_cycle)
-            time.sleep(0.02)
-        time.sleep(1)
+for _ in range(1):
+    for duty_cycle in range(50, 250, 5):  # Adjust values to match your servo's specifications
+        wpi.pwmWrite(p, duty_cycle)
+        time.sleep(0.02)
+    time.sleep(1)
+    for duty_cycle in range(250, 50, -5):  # Adjust values to match your servo's specifications
+        wpi.pwmWrite(p, duty_cycle)
+        time.sleep(0.02)
+    time.sleep(1)
