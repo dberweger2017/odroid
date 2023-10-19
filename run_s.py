6b789ad376ef140c7e5f585bedf10f8e460f947a
dberweger2017 <dberweger@gmail.com>
2023-10-19 14:05:31 +0200

    new

--- run_s.py
+++ run_s.py
@@ -1,16 +1,28 @@
 import streamlit as st
-import numpy as np
 import odroid_wiringpi as wpi
-
-st.title("Servo control")
-
-angle = st.slider("Angle", 0, 180, 90)
 
 # Initialize WiringPi
 wpi.wiringPiSetup()
 
+# Set the pin number (assuming pin 33, you might need to adjust this)
 pin = 1
 
+# Create a software-controlled PWM
 wpi.softPwmCreate(pin, 0, 200)
-duty_cycle = int((angle / 180.0) * 200)
-wpi.pinMode(pin, wpi.INPUT)
+
+st.title("Servo Controller")
+
+min_1 = 0
+max_1 = 30
+
+# Streamlit slider
+angle_to_move = st.slider("Select angle to move to:", min_value=min_1, max_value=max_1, step=1)
+
+try:
+    # Move to specific angle
+    duty_cycle = int((angle_to_move / 180.0) * 200)
+    wpi.softPwmWrite(pin, duty_cycle)
+    st.write(f"Moved to angle: {angle_to_move}")
+
+except Exception as e:
+    st.write(f"An error occurred: {e}")
